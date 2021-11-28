8d3386c543e2e28ea9d59de0bcf33e32e33cd793
Leif T. Sundkvist <leif.t.sundqvist@gmail.com>
2021-11-28 22:32:11 +0100

    setup functional CORS policy and made some adjustments to frontend.

--- coach-frontend/src/components/pages/game/index.tsx
+++ coach-frontend/src/components/pages/game/index.tsx
@@ -6,17 +6,24 @@ import { Paragraph, Page } from '../..';
 
 const axios = require('axios');
 
+interface Round {
+    readonly round: number;
+    readonly description: string;
+}
+
 interface Game {
     readonly id: number;
-    readonly name: string;
     readonly date: string;
+    readonly map: string;
+    rounds: Round[];
 }
 
 const games: Game[] = [1, 2, 3, 4, 5].map((i: number) => {
     return {
         id: i,
-        name: `Test game ${i}`,
         date: new Date().toDateString(),
+        rounds: [],
+        map: "korv me bröd",
     }
 })
 
@@ -38,16 +45,28 @@ const useStyles = makeStyles((theme : any) => ({
 export const GameListPage = (props: any) => {
     const navigate = useNavigate();
     const classes = useStyles();
+    const [games, setGames] = useState([]);
 
-    const gameItems = games.map(game => {
+    useEffect(() => {
+        axios.get('http://localhost:8080/games')
+        .then((response: any) => {
+            setGames(response.data);
+        })
+        .then((error: any) => {
+            console.log(error);
+        });
+    }, []);
+
+
+    const gameItems = games.map((game: Game) => {
         return <div 
         className={classes.gameCard} 
         key={game.id}
         onClick={() => navigate(`/games/${game.id}`)}
         >
             <Paragraph variant="h5">{game.id}</Paragraph>
-            <Paragraph variant="h6">{game.date}</Paragraph>
-            <Paragraph variant="h7">{game.name}</Paragraph>
+            {/* <Paragraph variant="h6">{game.date}</Paragraph>
+            <Paragraph variant="h7">{game.name}</Paragraph> */}
             {/* <Link to={`/games/${game.id}`}>View details</Link> */}
         </div>
     })
@@ -67,11 +86,10 @@ export const GameDetailsPage = (props: any) => {
     const [game, setGame] = useState<Game | undefined>(undefined);
 
     useEffect(() => {
-            axios.get(`http://localhost:8080/game/${id}`)
-            .then((response: Response)=> {
-                console.log(response);
-                // const newGame: (Game | undefined) = games.find((g: Game) => g.id === Number(id));
-                // setGame(newGame);
+            axios.get(`http://localhost:8080/games/${id}`)
+            .then((response: any)=> {
+                console.log(response.data);
+                setGame(response.data)
             })
             .catch((error: any) => {
                 console.log(error);
@@ -80,11 +98,19 @@ export const GameDetailsPage = (props: any) => {
 
     if(!game) return <></>;
 
+
+    const roundItems = game.rounds.map(round => {
+        return (
+            <Paragraph variant="h6">{`Round ${round.round} - ${round.description}`}</Paragraph>
+        )
+    })
+
     return (
         <Page>
             <Paragraph variant="h6">{game.id}</Paragraph>
-            <Paragraph variant="h3">{game.name}</Paragraph>
-            <Paragraph variant="h5">{game.date}</Paragraph>
+            <Paragraph variant="h3">{game.date}</Paragraph>
+            <Paragraph variant="h3">{game.map}</Paragraph>
+            {roundItems}
         </Page>
     )
 }
